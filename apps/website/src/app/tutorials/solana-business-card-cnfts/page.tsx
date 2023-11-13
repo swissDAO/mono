526bc82a5dfe17627f6d232941e7ab98e48f73ad
Matt Weichel <maweiche@gmail.com>
2023-11-13 21:17:36 +0100

    added side nav based on title of md files

--- apps/website/src/app/tutorials/solana-business-card-cnfts/page.tsx
+++ apps/website/src/app/tutorials/solana-business-card-cnfts/page.tsx
@@ -6,6 +6,7 @@ import style from '../../../../styles/markdown.module.css';
 
 const Tutorial: React.FC = () => {
   const [loading, setLoading] = useState(true);
+  const [mapJson, setMapJson] = useState({} as any);
   const [urlIndex, setUrlIndex] = useState(0);
   const [urlList, setUrlList] = useState<string[]>([]);
   const [markdown, setMarkdown] = useState('');
@@ -16,7 +17,7 @@ const Tutorial: React.FC = () => {
       'https://raw.githubusercontent.com/swissDAO/solana-business-card-cnfts/main/map.json'
     );
     const map_json = await map.json();
-    console.log(map_json);
+    setMapJson(map_json);
 
     const urls = Object.keys(map_json).map(key => {
       return `https://raw.githubusercontent.com/swissDAO/solana-business-card-cnfts/main/${key.slice(
@@ -25,14 +26,12 @@ const Tutorial: React.FC = () => {
       )}/${map_json[key]}`;
     });
     setUrlList(urls);
-    console.log(urlList);
     setLoading(false);
   }
 
   const fetchMarkdownData = async () => {
     const res = await fetch(urlList[urlIndex]);
     const markdown = await res.text();
-    console.log(markdown);
     setMarkdown(markdown);
   };
 
@@ -72,62 +71,84 @@ const Tutorial: React.FC = () => {
 
   return (
     <div className={style.parentContainer}>
-      <div className={style.buttonContainer}>
-        {urlIndex > 0 ? (
-          <button
-            onClick={() => {
-              setUrlIndex(urlIndex - 1);
-              fetchMarkdownData();
-            }}
-          >
-            Previous
-          </button>
-        ) : (
-          <></>
-        )}
-        {urlIndex < urlList.length - 1 ? (
-          <button
-            onClick={() => {
-              setUrlIndex(urlIndex + 1);
-              fetchMarkdownData();
-            }}
-          >
-            Next
-          </button>
-        ) : (
-          <></>
-        )}
+      <div
+        className={style.sideNavContainer}
+      >
+        {/* map the mapJson keys */}
+        {Object.keys(mapJson).map(key => {
+          return (
+            <div
+              className={style.sideNavButton}
+              onClick={() => {
+                setUrlIndex(Object.keys(mapJson).indexOf(key));
+                fetchMarkdownData();
+              }}
+            >            
+              <li>{mapJson[key].slice(9, -3).replace(/_/g, ' ')}</li>
+            </div>
+          );
+        })}
       </div>
-      {loading && styledMarkdown != '' ? (
-        <div>Loading...</div>
-      ) : (
-        renderMarkdown()
-      )}
-      <div className={style.buttonContainer}>
-        {urlIndex > 0 ? (
-          <button
-            onClick={() => {
-              setUrlIndex(urlIndex - 1);
-              fetchMarkdownData();
-            }}
-          >
-            Previous
-          </button>
+      <div
+        className={style.tutorialContainer}
+      >
+        <div className={style.buttonContainer}>
+          {urlIndex > 0 ? (
+            <button
+              onClick={() => {
+                setUrlIndex(urlIndex - 1);
+                fetchMarkdownData();
+              }}
+            >
+              Previous
+            </button>
+          ) : (
+            <></>
+          )}
+          {urlIndex < urlList.length - 1 ? (
+            <button
+              onClick={() => {
+                setUrlIndex(urlIndex + 1);
+                fetchMarkdownData();
+              }}
+            >
+              Next
+            </button>
+          ) : (
+            <></>
+          )}
+        </div>
+        {loading && styledMarkdown != '' ? (
+          <div>Loading...</div>
         ) : (
-          <></>
+          renderMarkdown()
         )}
-        {urlIndex < urlList.length - 1 ? (
-          <button
-            onClick={() => {
-              setUrlIndex(urlIndex + 1);
-              fetchMarkdownData();
-            }}
-          >
-            Next
-          </button>
-        ) : (
-          <></>
-        )}
+        <div className={style.buttonContainer}>
+          {urlIndex > 0 ? (
+            <button
+              onClick={() => {
+                setUrlIndex(urlIndex - 1);
+                fetchMarkdownData();
+              }}
+            >
+              Previous
+            </button>
+          ) : (
+            <></>
+          )}
+          {urlIndex < urlList.length - 1 ? (
+            <button
+              onClick={() => {
+                setUrlIndex(urlIndex + 1);
+                fetchMarkdownData();
+              }}
+            >
+              Next
+            </button>
+          ) : (
+            <></>
+          )}
+        </div>
       </div>
     </div>
   );
